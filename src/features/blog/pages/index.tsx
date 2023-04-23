d6a0defb3186b1a25d30939162a27bb68fc3e990
伊東和成 <itokazunari5375@icloud.com>
2023-04-23 09:05:55 +0900

    feat: カテゴリー検索関連の実装

--- src/features/blog/pages/index.tsx
+++ src/features/blog/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 import logo from '../../../../public/banner.png';
+import { FaPlus } from 'react-icons/fa';
 
 export const Blog: React.FC = () => {
   return (
@@ -9,8 +10,73 @@ export const Blog: React.FC = () => {
       <div className='banner'>
         <Image src={logo} alt='ロゴ' className='logo-image' />
       </div>
+      <div className='blog-container'>
+        <div className='title-category'>
+          <h2>新着記事</h2>
+          {/* MEMO: 最初のリリース時はカテゴリー検索は不要 */}
+          <div className='categories'>
+            <div className='category '>
+              <button className='active'>全て</button>
+            </div>
+            <div className='category'>
+              <button>エンジニア</button>
+            </div>
+            <div className='category'>
+              <button>デザイナー</button>
+            </div>
+            <div className='category'>
+              <button>マーケティング</button>
+            </div>
+            <div className='category'>
+              <button className='add-button'>
+                <PlusIcon />
+                もっと見る
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  .title-category {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h2 {
+      color: #27262e;
+      font-weight: bold;
+      font-size: 24px;
+    }
+  }
+  .title-category {
+    margin-top: 24px;
+  }
+  .categories {
+    display: flex;
+    gap: 24px;
+  }
+  .category {
+    color: #7a797d;
+    button {
+      padding: 5px 12px;
+      border-radius: 30px;
+    }
+    .active {
+      background-color: rgba(84, 41, 255, 0.1);
+    }
+  }
+  .add-button {
+    position: relative;
+  }
+`;
+
+const PlusIcon = styled(FaPlus)`
+  position: absolute;
+  left: -8px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #b9b8bc;
+`;
