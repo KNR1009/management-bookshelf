79eff8cf09cf45f70cced7602a6d59766b5a8e8f
伊東和成 <itokazunari5375@icloud.com>
2023-04-25 09:07:05 +0900

    feat: 画像の表示

--- src/features/blog/pages/[id].tsx
+++ src/features/blog/pages/[id].tsx
@@ -6,6 +6,14 @@ import Image from 'next/image';
 export const BlogDetail: React.FC = () => {
   return (
     <Wrapper>
+      <div className='title-container'>
+        <div className='category'>
+          <p>デザイン</p>
+        </div>
+        <div className='title-container'>
+          <h1>Sketch 67リリース。ドキュメントへの埋め込みフォント対応。</h1>
+        </div>
+      </div>
       <div className='image-container'>
         <img src='/mako.jpg' alt='' />
       </div>
@@ -14,10 +22,25 @@ export const BlogDetail: React.FC = () => {
 };
 
 const Wrapper = styled.div`
+  .category {
+    p {
+      color: #20528f;
+      font-size: 16px;
+      font-weight: bold;
+    }
+  }
+  .title-container {
+    h1 {
+      font-size: 36px;
+      font-weight: bold;
+      color: #062a4a;
+    }
+  }
   .image-container {
     width: 100%;
     height: 500px;
     overflow: hidden;
+    margin-top: 24px;
     img {
       width: 100%;
       height: 100%;
