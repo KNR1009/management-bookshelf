dd7846bfa7fc521a20492fabfd9e1ccc80314332
伊東和成 <itokazunari5375@icloud.com>
2023-04-30 17:24:12 +0900

    fix: 一部調整

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,28 +1,27 @@
-import type { NextPage } from 'next';
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 // page
-// import { BlogDetail } from '@/features/blog/pages/[id]';
+import { BlogDetail } from '@/features/blog/pages/[id]';
 // components
 import { Layout } from '@/components/layout';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
-// import Head from 'next/head';
-// import { BlogFactory, BlogType } from '@/model/blog';
-// import { RecommendationFactory } from '@/model/recommendation';
+import Head from 'next/head';
+import { BlogFactory, BlogType } from '@/model/blog';
+import { RecommendationFactory } from '@/model/recommendation';
 
-// type Props = {
-//   blog: BlogType;
-//   recommendations: BlogType[];
-// };
+type Props = {
+  blog: BlogType;
+  recommendations: BlogType[];
+};
 
-const Home: NextPage = () => {
-  // const router = useRouter();
+const Home: NextPage<Props> = ({ blog, recommendations }) => {
+  const router = useRouter();
 
-  // const currentUrl = process.browser ? window.location.origin + router.asPath : '';
+  const currentUrl = process.browser ? window.location.origin + router.asPath : '';
 
   return (
     <Layout>
-      <p>aaa</p>
-      {/* {blog && recommendations && (
+      {blog && recommendations && (
         <>
           <Head>
             <title>{blog.title}</title>
@@ -51,34 +50,35 @@ const Home: NextPage = () => {
           </Head>
           <BlogDetail blog={blog} recommendations={recommendations} />
         </>
-      )} */}
+      )}
     </Layout>
   );
 };
 
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const blogs = await BlogFactory().index();
-//   const paths = blogs.map((blog: any) => ({ params: { id: blog.id.toString() } }));
+export const getStaticPaths: GetStaticPaths = async () => {
+  const blogs = await BlogFactory().index();
+  const paths = blogs.map((blog: BlogType) => ({ params: { id: blog.id.toString() } }));
 
-//   return { paths, fallback: 'blocking' };
-// };
+  return { paths, fallback: false };
+};
 
-// export const getStaticProps: GetStaticProps<Props, { id: string }> = async ({ params }) => {
-//   const id = params?.id;
-//   if (!id) {
-//     return { notFound: true };
-//   }
+export const getStaticProps: GetStaticProps<Props, { id: string }> = async ({ params }) => {
+  const id = params?.id;
 
-//   try {
-//     const [blog, recommendations] = await Promise.all([
-//       BlogFactory().show(Number(id)),
-//       RecommendationFactory().index()
-//     ]);
-//     return { props: { blog, recommendations }, revalidate: 60 };
-//   } catch (error) {
-//     console.error(error);
-//     return { notFound: true };
-//   }
-// };
+  if (!id) {
+    return { notFound: true };
+  }
+
+  try {
+    const [blog, recommendations] = await Promise.all([
+      BlogFactory().show(Number(id)),
+      RecommendationFactory().index()
+    ]);
+    return { props: { blog, recommendations }, revalidate: 60 };
+  } catch (error) {
+    console.error(error);
+    return { notFound: true };
+  }
+};
 
 export default Home;
