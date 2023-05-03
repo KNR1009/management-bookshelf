f69a258fe8778c22e545ed7010c947f519286bb9
伊東和成 <itokazunari5375@icloud.com>
2023-05-03 15:43:33 +0900

    feat: Googleアナリティクスの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,45 @@ import '../styles/globals.css';
 import '../styles/reset.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { pageview } from '../src/lib/gtag';
+
+declare const window: Window & { gtag: any };
+
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
-        {/* <meta name='description' content='ページの説明文' />
-        <meta name='keywords' content='キーワード1, キーワード2, キーワード3' /> */}
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+          }}
+        />
       </Head>
       <Component {...pageProps} />;
     </>
---
+++ src/lib/gtag.ts
@@ -0,0 +1,13 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+
+export const pageview = (url: string) => {
+  if (typeof window === 'undefined') return;
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url
+  });
+};
+
+export const event = ({ action, params }: { action: string; params: any }) => {
+  if (typeof window === 'undefined') return;
+  window.gtag('event', action, params);
+};
